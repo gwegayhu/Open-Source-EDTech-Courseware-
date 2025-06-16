e1e98857f53878943b86815bd0bfad60783ea5eb
Mike Dawson <mike@ustadmobile.com>
2025-06-16 23:07:52 +0400

    Remove acceptance of application/html

--- shared/src/jvmMain/kotlin/world/respect/domain/opds/validator/ValidateOpdsPublicationUseCase.kt
+++ shared/src/jvmMain/kotlin/world/respect/domain/opds/validator/ValidateOpdsPublicationUseCase.kt
@@ -246,8 +246,9 @@ class ValidateOpdsPublicationUseCase(
     }
 
     companion object {
-        val LEARNING_UNIT_MIME_TYPES = listOf("text/html", "application/xml", "application/html+xml",
-            "application/html")
+        //Acceptable mime types for HTML as per
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types#texthtml
+        val LEARNING_UNIT_MIME_TYPES = listOf("text/html", "application/xml", "application/html+xml")
 
         val LEARNING_UNIT_RESERVED_PARAMS = listOf(
             "respectLaunchVersion", "auth", "given_name", "locale", "endpoint",
