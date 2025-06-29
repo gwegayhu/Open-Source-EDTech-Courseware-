8cae029bc7fa35c0c956ff89cfdd48448e626476
Mike Dawson <mike@ustadmobile.com>
2025-06-29 19:37:35 +0400

    Migrate use of kodein to koin.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,7 +36,11 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(projects.shared)
+
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
         }
+
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutines.swing)
