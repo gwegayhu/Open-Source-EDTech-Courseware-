1987723bf8e9e49750c54be61111bc7f6f687168
Mike Dawson <mike@ustadmobile.com>
2025-07-16 13:27:03 +0400

    Add initial logic to launch Respect compatible app as an intent.

--- respect-lib-shared/src/androidMain/kotlin/world/respect/shared/domain/launchapp/LaunchAppUseCaseAndroid.kt
+++ respect-lib-shared/src/androidMain/kotlin/world/respect/shared/domain/launchapp/LaunchAppUseCaseAndroid.kt
@@ -1,11 +1,13 @@
 package world.respect.shared.domain.launchapp
 
 import android.content.Context
+import android.content.Intent
 import io.ktor.http.Url
 import world.respect.datalayer.compatibleapps.model.RespectAppManifest
 import world.respect.shared.domain.account.RespectAccount
 import world.respect.shared.navigation.LearningUnitViewer
 import world.respect.shared.navigation.NavCommand
+import androidx.core.net.toUri
 
 class LaunchAppUseCaseAndroid(
     private val appContext: Context,
@@ -17,11 +19,28 @@ class LaunchAppUseCaseAndroid(
         learningUnitId: Url?,
         navigateFn: (NavCommand) -> Unit
     ) {
+        val androidPackageId = app.android?.packageId
+        val launchUrl = learningUnitId ?: Url(app.defaultLaunchUri.toString())
+
+        if(androidPackageId != null) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.setPackage(androidPackageId)
+            intent.setData(launchUrl.toString().toUri())
+
+            try {
+                appContext.startActivity(intent)
+                return
+            }catch(e: Throwable) {
+                e.printStackTrace()
+                //In future - apps will need an action we can query.
+                //didn't work - maybe wasn't installed.
+            }
+        }
+
         navigateFn(
             NavCommand.Navigate(
-                LearningUnitViewer.create(
-                    learningUnitId = learningUnitId ?: Url(app.defaultLaunchUri.toString())
-                )
+                LearningUnitViewer.create(learningUnitId = launchUrl)
             )
         )
     }
--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/learningunit/viewer/LearningUnitViewerViewModel.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/learningunit/viewer/LearningUnitViewerViewModel.kt
@@ -4,16 +4,12 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.toRoute
 import io.ktor.http.Url
 import kotlinx.coroutines.flow.MutableStateFlow
-import world.respect.shared.generated.resources.Res
-import world.respect.shared.generated.resources.loading
+import kotlinx.coroutines.flow.update
 import world.respect.shared.navigation.LearningUnitViewer
-import world.respect.shared.resources.StringResourceUiText
-import world.respect.shared.resources.UiText
 import world.respect.shared.viewmodel.RespectViewModel
 
 data class LearningUnitViewerViewModelUiState(
     val url: Url,
-    val title: UiText,
 )
 
 class LearningUnitViewerViewModel(
@@ -25,12 +21,16 @@ class LearningUnitViewerViewModel(
     val uiState = MutableStateFlow(
         LearningUnitViewerViewModelUiState(
             url = route.learningUnitId,
-            title = StringResourceUiText(Res.string.loading)
         ),
     )
 
     init {
-
+        _appUiState.update {
+            it.copy(
+                title = "",
+                navigationVisible = false,
+            )
+        }
     }
 
 }
