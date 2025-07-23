74f126900ba72c41e81d6a5f8557f1500792c5e0
Mike Dawson <mike@ustadmobile.com>
2025-07-23 08:03:58 +0300

    Add basic logic in account manager.

--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/domain/account/RespectAccountManager.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/domain/account/RespectAccountManager.kt
@@ -1,10 +1,12 @@
 package world.respect.shared.domain.account
 
 import com.russhwolf.settings.Settings
+import com.russhwolf.settings.set
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.updateAndGet
 import kotlinx.serialization.json.Json
 import org.koin.core.component.KoinComponent
 
@@ -25,10 +27,29 @@ class RespectAccountManager(
         settings.getStringOrNull(SETTINGS_KEY_ACTIVE_ACCOUNT)
     )
 
-    private val activeAccount: RespectAccount?
+    var activeAccount: RespectAccount?
         get() = _storedAccounts.value.firstOrNull {
             it.userSourcedId == _activeAccountSourcedId.value
         }
+
+        set(value) {
+            if(value == null) {
+                _activeAccountSourcedId.value = null
+                settings.remove(SETTINGS_KEY_ACTIVE_ACCOUNT)
+            }else {
+                if(!_storedAccounts.value.any { it.userSourcedId == value.userSourcedId }) {
+                    val newValue = _storedAccounts.updateAndGet { prev ->
+                        listOf(value) + prev
+                    }
+                    settings[SETTINGS_KEY_STORED_ACCOUNTS] = json.encodeToString(newValue)
+                }
+
+                _activeAccountSourcedId.value = value.userSourcedId
+                settings.set(SETTINGS_KEY_ACTIVE_ACCOUNT, value.userSourcedId)
+            }
+        }
+
+
 
     private val activeAccountFlow: Flow<RespectAccount?> = _storedAccounts.combine(
         _activeAccountSourcedId
