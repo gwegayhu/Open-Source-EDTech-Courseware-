bb99e0d9737d8a6f9ebe5dc1d2446717a285e714
Mandvii <mandvi.verma@sutara.org>
2025-07-28 11:55:40 +0530

    Added class screen

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/view/clazz/addclazz/AddClazzScreen.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/view/clazz/addclazz/AddClazzScreen.kt
@@ -1,9 +1,60 @@
 package world.respect.app.view.clazz.addclazz
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import world.respect.shared.viewmodel.clazz.addclazz.AddClazzViewModel
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.stringResource
+import world.respect.shared.generated.resources.Res
+import world.respect.shared.generated.resources.class_name_label
+import world.respect.shared.viewmodel.clazz.addclazz.AddClazzUiState
 
-class AddClazzScreen(
+
+@Composable
+fun AddClazzScreen(
     viewModel: AddClazzViewModel
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    AddClazzScreen(
+        uiState = uiState,
+        onClassNameChange = viewModel::onClassNameChange,
+        )
+}
+
+@Composable
+fun AddClazzScreen(
+    uiState: AddClazzUiState,
+    onClassNameChange: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        OutlinedTextField(
+            value = uiState.className,
+            onValueChange = onClassNameChange,
+            label = {
+                Text(
+                    text = stringResource(Res.string.class_name_label)
+                )
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+    }
+
 
 }
--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/clazz/addclazz/AddClazzViewModel.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/clazz/addclazz/AddClazzViewModel.kt
@@ -2,23 +2,35 @@ package world.respect.shared.viewmodel.clazz.addclazz
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.getString
 import world.respect.shared.generated.resources.Res
 import world.respect.shared.generated.resources.save
+import world.respect.shared.generated.resources.classes
 import world.respect.shared.viewmodel.RespectViewModel
 import world.respect.shared.viewmodel.app.appstate.ActionBarButtonUiState
+
+data class AddClazzUiState(
+    var className: String = "",
+)
 
 class AddClazzViewModel(
     savedStateHandle: SavedStateHandle,
 ) : RespectViewModel(savedStateHandle) {
 
+    private val _uiState = MutableStateFlow(AddClazzUiState())
+
+    val uiState = _uiState.asStateFlow()
+
     init {
         viewModelScope.launch {
             _appUiState.update { prev ->
                 prev.copy(
-                    userAccountIconVisible=false,
+                    title = getString(Res.string.classes),
+                    userAccountIconVisible = false,
                     actionBarButtonState = ActionBarButtonUiState(
                         visible = true,
                         text = getString(Res.string.save)
@@ -26,5 +38,11 @@ class AddClazzViewModel(
                 )
             }
         }
+    }
+
+    fun onClassNameChange(
+        className: String
+    ) {
+
     }
 }
