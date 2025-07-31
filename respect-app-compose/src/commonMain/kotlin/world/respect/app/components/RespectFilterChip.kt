b70df82679d6842f419d2de9d31ce8d71d71e0ad
Mandvii <mandvi.verma@sutara.org>
2025-07-31 13:31:32 +0530

    FilterChip and SortOption

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/components/RespectFilterChip.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/components/RespectFilterChip.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 
@@ -35,21 +34,13 @@ fun <T> RespectFilterChip(
                 label = {
                     Text(
                         text = optionLabel(option),
-                        style = MaterialTheme.typography.bodyMedium.copy(
-                            fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
-                            color = if (isSelected) Color.Black else Color.Gray
-                        )
+                        color = if (isSelected) Color.Black else Color.Gray
+
                     )
                 },
                 border = FilterChipDefaults.filterChipBorder(
                     enabled = true,
                     selected = selectedOption == option,
-                    borderColor = Color.Gray,
-                    selectedBorderColor = Color.Black,
-                    disabledBorderColor = Color.LightGray,
-                    disabledSelectedBorderColor = Color.DarkGray,
-                    borderWidth = 1.dp,
-                    selectedBorderWidth = 2.dp
                 )
 
             )
