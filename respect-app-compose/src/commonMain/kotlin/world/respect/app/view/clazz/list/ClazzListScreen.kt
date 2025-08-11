d4b7d7bea2cedf9903192458f88d8f5d825c4b72
Mandvii <mandvi.verma@sutara.org>
2025-08-12 00:09:04 +0530

    Removed unused import from ClazzListScreen

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/view/clazz/list/ClazzListScreen.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/view/clazz/list/ClazzListScreen.kt
@@ -3,7 +3,6 @@ package world.respect.app.view.clazz.list
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ListItem
