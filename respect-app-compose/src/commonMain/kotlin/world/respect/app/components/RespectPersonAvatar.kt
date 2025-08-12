aca85a2586636c67bcbd3bba1baf0b953369fab8
Mandvii <mandvi.verma@sutara.org>
2025-08-12 15:49:13 +0530

    RespectPersonAvatar component updated
    The `RespectPersonAvatar` component now accepts `firstName` and `lastName` parameters directly, instead of a `RespectPerson` object. This simplifies the component's API and makes it more reusable.
    
    The component's logic for generating the avatar color and initials has been updated to use the new `firstName` and `lastName` parameters.

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/components/RespectPersonAvatar.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/components/RespectPersonAvatar.kt
@@ -3,14 +3,12 @@ package world.respect.app.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.unit.dp
 import world.respect.app.util.ext.rgbaColor
 import world.respect.shared.util.avatarColorForName
 import world.respect.shared.util.initial
