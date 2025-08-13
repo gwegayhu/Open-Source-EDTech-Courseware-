ff79c3d06bcf400181eca901568bcae7185af236
Mike Dawson <mike@ustadmobile.com>
2025-08-13 16:48:05 +0400

    Add Exception utility classes.

---
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/util/exception/ExceptionWithHttpStatusCode.kt
@@ -0,0 +1,26 @@
+package world.respect.shared.util.exception
+
+/**
+ * Some exceptions map to a particular http status code This interface makes it easier for http
+ * server and client components to handle exceptions e.g. an http server's exception handling can
+ * simply catch the exception and then set the status code directly.
+ */
+interface ExceptionWithHttpStatusCode {
+
+    val statusCode: Int
+
+}
+
+class ExceptionWithHttpStatusCodeWrapper internal constructor(
+    cause: Throwable?,
+    message: String?,
+    override val statusCode: Int
+): Exception(message, cause), ExceptionWithHttpStatusCode
+
+fun Throwable.withHttpStatusCode(statusCode: Int): Exception {
+    return ExceptionWithHttpStatusCodeWrapper(this, message, statusCode)
+}
+
+fun Throwable.getHttpStatusCode(): Int? {
+    return getCauseOfType<ExceptionWithHttpStatusCode>()?.statusCode
+}
